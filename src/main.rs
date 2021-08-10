15354065334b793b9f5c205bb9b410b8b1ca6b4f
Snehit Sah <snehitsah@protonmail.com>
2021-08-10 19:15:34 +0530

    Ansi Term Version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use ansi_term::Color::Red;
+use sysinfo::{ComponentExt, ProcessorExt, System, SystemExt};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    println!("{} : {}", Red.bold().paint("OS"), sys.name().unwrap());
+    println!(
+        "{} : {}",
+        Red.bold().paint("Kernel"),
+        sys.kernel_version().unwrap()
+    );
+    println!(
+        "{} : {}",
+        Red.bold().paint("Host"),
+        sys.host_name().unwrap()
+    );
+
+    println!(
+        "{} : {}",
+        Red.bold().paint("CPU"),
+        sys.processors()[0].brand()
+    );
+    println!("{} : {}", Red.bold().paint("Cores"), sys.processors().len());
+
+    let mut usage = 0;
+
+    for processor in sys.processors() {
+        usage = usage + processor.cpu_usage() as usize;
+    }
+    println!(
+        "{} : {}%",
+        Red.bold().paint("Usage"),
+        usage / sys.processors().len()
+    );
+
+    for component in sys.components() {
+        if component.label().contains("Package id") {
+            println!(
+                "{} : {}C",
+                Red.bold().paint("Temp"),
+                component.temperature()
+            );
+        }
+    }
+
+    println!(
+        "{} : {}MB/{}MB",
+        Red.bold().paint("Mem"),
+        sys.used_memory() / 1024,
+        sys.total_memory() / 1024
+    );
+    println!(
+        "{} : {}MB/{}MB",
+        Red.bold().paint("Swap"),
+        sys.used_swap() / 1024,
+        sys.total_swap() / 1024
+    );
 }
