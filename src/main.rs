b6cb97b0f1a7f54d62e0d57c003c626a5085c7ae
Snehit Sah <snehitsah@protonmail.com>
2021-08-10 19:17:14 +0530

    Add carriage return and use raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
 use ansi_term::Color::Red;
 use sysinfo::{ComponentExt, ProcessorExt, System, SystemExt};
-
+use termion::{color, style};
+use termion::{cursor::DetectCursorPos, raw::IntoRawMode};
 fn main() {
+    let left_pad = 30;
+    let mut stdout = std::io::stdout().into_raw_mode().unwrap();
+    // println!("{}\r", stdout.cursor_pos().unwrap().1);
+
     let mut sys = System::new_all();
     sys.refresh_all();
 
-    println!("{} : {}", Red.bold().paint("OS"), sys.name().unwrap());
+    println!("{} : {}\r", Red.bold().paint("OS"), sys.name().unwrap());
     println!(
-        "{} : {}",
+        "{} : {}\r",
         Red.bold().paint("Kernel"),
         sys.kernel_version().unwrap()
     );
     println!(
-        "{} : {}",
+        "{} : {}\r",
         Red.bold().paint("Host"),
         sys.host_name().unwrap()
     );
 
     println!(
-        "{} : {}",
+        "{} : {}\r",
         Red.bold().paint("CPU"),
         sys.processors()[0].brand()
     );
-    println!("{} : {}", Red.bold().paint("Cores"), sys.processors().len());
+    println!(
+        "{} : {}\r",
+        Red.bold().paint("Cores"),
+        sys.processors().len()
+    );
 
     let mut usage = 0;
 
@@ -30,7 +39,7 @@ fn main() {
         usage = usage + processor.cpu_usage() as usize;
     }
     println!(
-        "{} : {}%",
+        "{} : {}%\r",
         Red.bold().paint("Usage"),
         usage / sys.processors().len()
     );
@@ -38,7 +47,7 @@ fn main() {
     for component in sys.components() {
         if component.label().contains("Package id") {
             println!(
-                "{} : {}C",
+                "{} : {}C\r",
                 Red.bold().paint("Temp"),
                 component.temperature()
             );
@@ -46,13 +55,13 @@ fn main() {
     }
 
     println!(
-        "{} : {}MB/{}MB",
+        "{} : {}MB/{}MB\r",
         Red.bold().paint("Mem"),
         sys.used_memory() / 1024,
         sys.total_memory() / 1024
     );
     println!(
-        "{} : {}MB/{}MB",
+        "{} : {}MB/{}MB\r",
         Red.bold().paint("Swap"),
         sys.used_swap() / 1024,
         sys.total_swap() / 1024
