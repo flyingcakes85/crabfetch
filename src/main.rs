05b0053a19516264212411d25052dcef346831cb
michal <michal@tar.black>
2021-12-04 04:24:57 +0000

    general maintenance + linting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use ansi_term::Color::Red;
 use sysinfo::{ComponentExt, ProcessorExt, System, SystemExt};
 fn main() {
-    println!(" ");
     let left_pad = 25;
     let mut sys = System::new_all();
     sys.refresh_all();
@@ -9,21 +8,21 @@ fn main() {
     println!("{}",Red.paint("     /\\\r\n    ( /   @ @    ()\r\n     \\  __| |__  /\r\n      -/   \"   \\-\r\n     /-|       |-\\\r\n    / /-\\     /-\\ \\\r\n     / /-`---\'-\\ \\\r\n      /         \\\r\n") );
 
     let mut cursor_row = crossterm::cursor::position().unwrap().1;
-    cursor_row = cursor_row - 9;
+    cursor_row -= 9;
     println!(
         "{}{} : {}\r",
         crossterm::cursor::MoveTo(left_pad, cursor_row),
         Red.bold().paint("OS"),
         sys.name().unwrap()
     );
-    cursor_row = cursor_row + 1;
+    cursor_row += 1;
     println!(
         "{}{} : {}\r",
         crossterm::cursor::MoveTo(left_pad, cursor_row),
         Red.bold().paint("Kernel"),
         sys.kernel_version().unwrap()
     );
-    cursor_row = cursor_row + 1;
+    cursor_row += 1;
 
     println!(
         "{}{} : {}\r",
@@ -31,7 +30,7 @@ fn main() {
         Red.bold().paint("Host"),
         sys.host_name().unwrap()
     );
-    cursor_row = cursor_row + 1;
+    cursor_row += 1;
 
     println!(
         "{}{} : {}\r",
@@ -39,7 +38,7 @@ fn main() {
         Red.bold().paint("CPU"),
         sys.processors()[0].brand()
     );
-    cursor_row = cursor_row + 1;
+    cursor_row += 1;
 
     println!(
         "{}{} : {}\r",
@@ -47,12 +46,12 @@ fn main() {
         Red.bold().paint("Cores"),
         sys.processors().len()
     );
-    cursor_row = cursor_row + 1;
+    cursor_row += 1;
 
     let mut usage = 0;
 
     for processor in sys.processors() {
-        usage = usage + processor.cpu_usage() as usize;
+        usage += processor.cpu_usage() as usize;
     }
     println!(
         "{}{} : {}%\r",
@@ -60,7 +59,7 @@ fn main() {
         Red.bold().paint("Usage"),
         usage / sys.processors().len()
     );
-    cursor_row = cursor_row + 1;
+    cursor_row += 1;
 
     for component in sys.components() {
         if component.label().contains("Package id") {
@@ -70,7 +69,7 @@ fn main() {
                 Red.bold().paint("Temp"),
                 component.temperature()
             );
-            cursor_row = cursor_row + 1;
+            cursor_row += 1;
             break;
         }
     }
@@ -82,7 +81,7 @@ fn main() {
         sys.used_memory() / 1024,
         sys.total_memory() / 1024
     );
-    cursor_row = cursor_row + 1;
+    cursor_row += 1;
 
     println!(
         "{}{} : {}MB/{}MB\r",
