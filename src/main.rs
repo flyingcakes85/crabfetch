eb4ceb0280715c871219ce7f628ca693f3283047
Snehit Sah <snehitsah@protonmail.com>
2024-05-02 19:17:37 +0530

    can reorder or omit modules
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -108,103 +108,122 @@ fn main() {
 
     let mut cursor_row = stdout.cursor_pos().unwrap().1;
     cursor_row = cursor_row - 9;
-    println!(
-        "{}{} : {}\r",
-        termion::cursor::Goto(left_pad, cursor_row),
-        Red.bold().paint(cfg.custom_labels.os),
-        cfg.custom_parameters.os.unwrap_or(sys.name().unwrap())
-    );
-    cursor_row = cursor_row + 1;
-    println!(
-        "{}{} : {}\r",
-        termion::cursor::Goto(left_pad, cursor_row),
-        Red.bold().paint(cfg.custom_labels.kernel),
-        cfg.custom_parameters
-            .kernel
-            .unwrap_or(sys.kernel_version().unwrap())
-    );
-    cursor_row = cursor_row + 1;
 
-    println!(
-        "{}{} : {}\r",
-        termion::cursor::Goto(left_pad, cursor_row),
-        Red.bold().paint(cfg.custom_labels.host),
-        cfg.custom_parameters
-            .host
-            .unwrap_or(sys.host_name().unwrap())
-    );
-    cursor_row = cursor_row + 1;
+    for module in &cfg.modules {
+        match module {
+            Module::OS => {
+                println!(
+                    "{}{} : {}\r",
+                    termion::cursor::Goto(left_pad, cursor_row),
+                    Red.bold().paint(&cfg.custom_labels.os),
+                    cfg.custom_parameters
+                        .os
+                        .clone()
+                        .unwrap_or(sys.name().unwrap())
+                );
+            }
+            Module::Kernel => {
+                println!(
+                    "{}{} : {}\r",
+                    termion::cursor::Goto(left_pad, cursor_row),
+                    Red.bold().paint(&cfg.custom_labels.kernel),
+                    cfg.custom_parameters
+                        .kernel
+                        .clone()
+                        .unwrap_or(sys.kernel_version().unwrap())
+                );
+            }
+            Module::Host => {
+                println!(
+                    "{}{} : {}\r",
+                    termion::cursor::Goto(left_pad, cursor_row),
+                    Red.bold().paint(&cfg.custom_labels.host),
+                    cfg.custom_parameters
+                        .host
+                        .clone()
+                        .unwrap_or(sys.host_name().unwrap())
+                );
+            }
+            Module::CPU => {
+                println!(
+                    "{}{} : {}\r",
+                    termion::cursor::Goto(left_pad, cursor_row),
+                    Red.bold().paint(&cfg.custom_labels.cpu),
+                    cfg.custom_parameters
+                        .cpu
+                        .clone()
+                        .unwrap_or(sys.processors()[0].brand().to_string())
+                );
+            }
+            Module::Cores => {
+                println!(
+                    "{}{} : {}\r",
+                    termion::cursor::Goto(left_pad, cursor_row),
+                    Red.bold().paint(&cfg.custom_labels.cores),
+                    cfg.custom_parameters
+                        .cores
+                        .clone()
+                        .unwrap_or(sys.processors().len().to_string())
+                );
+            }
+            Module::Usage => {
+                let mut usage = 0;
 
-    println!(
-        "{}{} : {}\r",
-        termion::cursor::Goto(left_pad, cursor_row),
-        Red.bold().paint(cfg.custom_labels.cpu),
-        cfg.custom_parameters
-            .cpu
-            .unwrap_or(sys.processors()[0].brand().to_string())
-    );
-    cursor_row = cursor_row + 1;
-
-    println!(
-        "{}{} : {}\r",
-        termion::cursor::Goto(left_pad, cursor_row),
-        Red.bold().paint(cfg.custom_labels.cores),
-        cfg.custom_parameters
-            .cores
-            .unwrap_or(sys.processors().len().to_string())
-    );
-    cursor_row = cursor_row + 1;
-
-    let mut usage = 0;
-
-    for processor in sys.processors() {
-        usage = usage + processor.cpu_usage() as usize;
-    }
-    println!(
-        "{}{} : {}%\r",
-        termion::cursor::Goto(left_pad, cursor_row),
-        Red.bold().paint(cfg.custom_labels.usage),
-        cfg.custom_parameters
-            .usage
-            .unwrap_or((usage / sys.processors().len()).to_string())
-    );
-    cursor_row = cursor_row + 1;
-
-    for component in sys.components() {
-        if component.label().contains("Package id") {
-            println!(
-                "{}{} : {}C\r",
-                termion::cursor::Goto(left_pad, cursor_row),
-                Red.bold().paint(cfg.custom_labels.temp),
-                cfg.custom_parameters
-                    .temp
-                    .unwrap_or(component.temperature().to_string())
-            );
-            cursor_row = cursor_row + 1;
-            break;
+                for processor in sys.processors() {
+                    usage = usage + processor.cpu_usage() as usize;
+                }
+                println!(
+                    "{}{} : {}%\r",
+                    termion::cursor::Goto(left_pad, cursor_row),
+                    Red.bold().paint(&cfg.custom_labels.usage),
+                    cfg.custom_parameters
+                        .usage
+                        .clone()
+                        .unwrap_or((usage / sys.processors().len()).to_string())
+                );
+            }
+            Module::Temp => {
+                for component in sys.components() {
+                    if component.label().contains("Package id") {
+                        println!(
+                            "{}{} : {}\r",
+                            termion::cursor::Goto(left_pad, cursor_row),
+                            Red.bold().paint(&cfg.custom_labels.temp),
+                            cfg.custom_parameters
+                                .temp
+                                .clone()
+                                .unwrap_or(format!("{}C",component.temperature().to_string()))
+                        );
+                        break;
+                    }
+                }
+            }
+            Module::Mem => {
+                println!(
+                    "{}{} : {}\r",
+                    termion::cursor::Goto(left_pad, cursor_row),
+                    Red.bold().paint(&cfg.custom_labels.mem),
+                    cfg.custom_parameters.mem.clone().unwrap_or(format!(
+                        "{}MB/{}MB",
+                        sys.used_memory() / 1024,
+                        sys.total_memory() / 1024
+                    ))
+                );
+            }
+            Module::Swap => {
+                println!(
+                    "{}{} : {}\r",
+                    termion::cursor::Goto(left_pad, cursor_row),
+                    Red.bold().paint(&cfg.custom_labels.swap),
+                    cfg.custom_parameters.swap.clone().unwrap_or(format!(
+                        "{}MB/{}MB",
+                        sys.used_swap() / 1024,
+                        sys.total_swap() / 1024
+                    ))
+                );
+            }
         }
+
+        cursor_row = cursor_row + 1;
     }
-
-    println!(
-        "{}{} : {}\r",
-        termion::cursor::Goto(left_pad, cursor_row),
-        Red.bold().paint(cfg.custom_labels.mem),
-        cfg.custom_parameters.mem.unwrap_or(format!(
-            "{}MB/{}MB",
-            sys.used_memory() / 1024,
-            sys.total_memory() / 1024
-        ))
-    );
-    cursor_row = cursor_row + 1;
-
-    println!(
-        "{}{} : {}\r",
-        termion::cursor::Goto(left_pad, cursor_row),
-        Red.bold().paint(cfg.custom_labels.swap),
-        cfg.custom_parameters.swap.unwrap_or(format!(
-            "{}MB/{}MB",
-            sys.used_swap() / 1024,
-            sys.total_swap() / 1024
-        ))
-    );
 }
